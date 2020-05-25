a8136ff5240ff59f119f557801117f7b0bcf35bd
1025manana1025 <61917491+1025manana1025@users.noreply.github.com>
2020-05-25 13:25:18 -0500

    Inital Build
    Calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,23 @@
+using System;
+namespace Services
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
